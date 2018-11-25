482168930c6865f11e98046d72c6a6c207a4ba91
nalorakq8 <yousefmon@gmail.com>
2018-11-25 11:11:18 +0300

    fixing issue

--- garage_sale/settings.py
+++ garage_sale/settings.py
@@ -133,8 +133,8 @@ USE_TZ = True
 EMAIL_USE_TLS = True
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
-EMAIL_HOST_USER = 'harayauction@gmail.com'
-EMAIL_HOST_PASSWORD = 'mdrmegbfunclqcki'
+EMAIL_HOST_USER = "**Hidden**"
+EMAIL_HOST_PASSWORD = "**Hidden**"
 DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
 AUTHENTICATION_BACKENDS = (
     "django.contrib.auth.backends.ModelBackend",
